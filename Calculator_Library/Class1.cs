f407178aebb124e0ece0c3190f7b270e6d52b259
Damodhar jadhao <Damodhar.jadhao@emtecinc.com>
2022-11-14 18:53:36 +0530

    Created a calculator using .Net Core WEB API.

---
+++ Calculator_Library/Class1.cs
@@ -0,0 +1,25 @@
+﻿namespace Calculator_Library
+{
+    public class Class1
+    {
+        public static string Add(int n1,int n2)
+        {
+            return $"Addition of {n1} and {n2} is : {n1 + n2}";
+        }
+
+        public static string Sub(int n1, int n2)
+        {
+            return $"Subtraction of {n1} and {n2} is : {n1 - n2}";
+        }
+
+        public static string Multi(int n1, int n2)
+        {
+            return $"Multiplication of {n1} and {n2} is : {n1 * n2}";
+        }
+
+        public static string Div(int n1, int n2)
+        {
+            return $"Division of {n1} and {n2} is : {n1 / n2}";
+        }
+    }
+}
---
+++ WebAPICore/Controllers/CalcController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAPICore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalcController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { 
+                Calculator_Library.Class1.Add(30,20),
+                Calculator_Library.Class1.Sub(30,20),
+                Calculator_Library.Class1.Multi(30,10),
+                Calculator_Library.Class1.Div(30,5)
+            };
+        }
+    }
+}
